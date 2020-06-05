2054b16128c151a8206f5f25c8034e3552ed44ea
Rodrigo Barros Bernardino <rbbernardino@gmail.com>
2020-06-04 22:26:25 -0300

    finished first fully functional version

--- main.ts
+++ main.ts
@@ -25,5 +25,37 @@ function include(filename: string) {
 }
 
 function ordenarPorValor() {
+  const comparadorSheet = SpreadsheetApp.getActive().getSheetByName("Comparador");
+  if (comparadorSheet) {
+    comparadorSheet.getRange("A3:C")
+      .sort([{ column: 3, ascending: true }]);
 
+    comparadorSheet.getRange("E3:G")
+      .sort([{ column: 5, ascending: true }]);
+  }
+}
+
+function alinharValores() {
+  const comparadorSheet = SpreadsheetApp.getActive().getSheetByName("Comparador");
+  if (comparadorSheet) {
+    const lastFilledBank = getLastFilledLine(comparadorSheet, "E", 3);
+    for (let curLn = 3; curLn < lastFilledBank; curLn++) {
+      const curMobillsValue: number = comparadorSheet.getRange(`C${curLn}`).getValue();
+      const curBankValue: number = comparadorSheet.getRange(`E${curLn}`).getValue();
+      if (Math.abs(curMobillsValue - curBankValue) > 0.02 && curMobillsValue > curBankValue) {
+        comparadorSheet.getRange(curLn, 1, 1, 3) // columns "A:C"
+          .insertCells(SpreadsheetApp.Dimension.ROWS);
+      }
+    }
+  }
+}
+
+function getLastFilledLine(sheet: GoogleAppsScript.Spreadsheet.Sheet, colLetter: string, startFrom = 0) {
+  var colData = sheet.getRange(colLetter + ":" + colLetter).getValues();
+  for (let i = startFrom; i < colData.length; i++) { // length --> height --> rows
+    const cellData = colData[i][0];
+    if (cellData == "")
+      return i;
+  }
+  return colData.length;
 }
